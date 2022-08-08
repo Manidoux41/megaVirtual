48d7255fcb31236faaaf776c960b879f384d36af
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2022-08-08 16:56:22 +0200

    modif front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App = () => {
     return (
         <div className='App'>
-            <h1>Mega Virtual Airline</h1>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
         </div>
     );
 };
